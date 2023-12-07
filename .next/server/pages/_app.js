85cb17bf2ea0d2056362f75ce102112ed750902b
ifatich <ifatich@gmail.com>
2023-12-07 11:16:20 +0700

    Remove node_modules from last commit

---
+++ .next/server/pages/_app.js
@@ -0,0 +1,1 @@
+"use strict";(()=>{var e={};e.id=888,e.ids=[888],e.modules={997:e=>{e.exports=require("react/jsx-runtime")}};var r=require("../webpack-runtime.js");r.C(e);var __webpack_exec__=e=>r(r.s=e),_=r.X(0,[637],()=>__webpack_exec__(1637));module.exports=_})();
---
+++ components/Button/Button.tsx
@@ -0,0 +1,38 @@
+// components/Button.tsx
+import React, { FC, MouseEvent } from 'react';
+
+interface ButtonProps {
+    label: string;
+    variant?: 'primary' | 'secondary' | 'disabled';
+    onClick?: (event: MouseEvent<HTMLButtonElement>) => void;
+}
+
+const styles = {
+
+    primary: {
+        backgroundColor: '#3498db',
+        color: '#fff',
+    },
+    secondary: {
+        backgroundColor: '#2ecc71',
+        color: '#fff',
+    },
+    disabled: {
+        backgroundColor: '#dcdcdc',
+        color: '#888',
+        cursor: 'not-allowed',
+    },
+};
+
+const Button: FC<ButtonProps> = ({ label, variant = 'primary', onClick }) => {
+    const buttonStyle = styles[variant] || {};
+
+    return (
+        <button style={{ padding: '10px 20px', fontSize: '16px', borderRadius: '4px', border: 'none', ...buttonStyle }}
+            onClick={onClick} disabled={variant==='disabled' }>
+            {label}
+        </button>
+    );
+};
+
+export default Button;
